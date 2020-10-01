503307dee0f64a03a7dc73e7533bfa6408bf57db
Вячеслав Филатов <Filatov_VV@live.ru>
2020-10-01 19:33:02 +0300

    add static sample

--- ConsoleApp5/ConsoleApp5/Program.cs
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -5,8 +5,37 @@ namespace ConsoleApp5
 {
     class Program
     {
+        class person
+        {
+            public static int bonus;
+            public string name;
+            public person() {
+                bonus = 0;
+                name = "fvv";
+            }
+            public void set_bonus(int s)
+            {
+                bonus = s;
+            }
+            public override string ToString()
+            {
+                string str1 = $"мой бонус  {bonus}";
+                return str1;
+            }
+        }
         static void Main(string[] args)
         {
+            person p1 = new person();
+            person p2 = new person();
+            person p3 = new person();
+            p3.set_bonus(15);
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            p1.set_bonus(55);
+            Console.WriteLine(p3);
+            int i = 0;
+            Console.WriteLine($"i = {i.ToString()}");
+            // add test fix
             Console.Beep(659, 125);
             Console.Beep(659, 125);
             Thread.Sleep(125);
